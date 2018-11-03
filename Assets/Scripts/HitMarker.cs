c8da3de55fd7a384347f4cb1dc6134ab4d53514a
aquacat <andernoha@gmail.com>
2018-11-03 11:37:49 -0400

    Initial commit

---
+++ Assets/Scripts/HitMarker.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+
+public class HitMarker : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private object other;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Dummie"))
+        {
+            other.gameObject.SetActive(false);
+        }
+		
+		if (other.gameObject.CompareTag("Dummie2"))
+        {
+            other.gameObject.SetActive(false);
+        }
+		
+		if (other.gameObject.CompareTag("Dummie3"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
